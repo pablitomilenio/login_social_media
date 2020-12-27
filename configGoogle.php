eba88ef971eacd77b0818b1c7bcd0609aa4a77d6
pablitomillenios <65786238+pablitomillenios@users.noreply.github.com>
2020-12-27 12:48:13 +0100

    .

--- configGoogle.php
+++ configGoogle.php
@@ -2,7 +2,7 @@
 require_once 'vendor/autoload.php';
  
 $config = [
-    'callback' => 'YOUR_DOMAIN_URL/index.php',
+    'callback' => 'https://www.free-okr.com/index.php',
     'keys'     => [
                     'id' => '36830912787-68ohb2u51ilj4pogla2kpilli4vip10n.apps.googleusercontent.com',
                     'secret' => 'b5EPiXGZGFRhsHtKO3CTpMWJ'
