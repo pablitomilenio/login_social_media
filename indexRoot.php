bd5381689645fbd6f217c45aae4f634d3204d720
pablitomillenios <65786238+pablitomillenios@users.noreply.github.com>
2020-12-27 15:14:10 +0100

    loginG

--- indexRoot.php
+++ indexRoot.php
@@ -12,7 +12,7 @@ Free-OKR
 
 Objectives and Key Results
 
-<a href=/login_social_media/loginLinkedIn.php><img src="signing-button-linkedin.png"></a>
+<a href=/login_social_media/loginLinkedIn.php><img src="signin-button-linkedin.png"></a>
 <br><br>
-<a href=/login_social_media/loginGoogle.php><img src="signing-button-google.png" width="220px"></a>
+<a href=/login_social_media/loginGoogle.php><img src="signin-button-google.png" width="220px"></a>
 </body>
