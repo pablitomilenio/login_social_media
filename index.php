49d258e1bc699e2977ebe4d977f2f40eb5824475
pablitomillenios <65786238+pablitomillenios@users.noreply.github.com>
2020-12-27 11:40:03 +0100

    .

--- index.php
+++ index.php
@@ -1,13 +1,8 @@
 <?php
-if (empty($_SERVER['HTTPS']) || $_SERVER['HTTPS'] === "off") {
-    $location = 'https://' . $_SERVER['HTTP_HOST'] . $_SERVER['REQUEST_URI'];
-    header('HTTP/1.1 301 Moved Permanently');
-    header('Location: ' . $location);
-    exit;
-}
+
 $config_file_path = 'https://www.free-okr.com/login_social_media/config.php';
  
-require_once( "https://www.free-okr.com/login_social_media/Auth.php" );
+require_once( "/path/to/hybridauth/Hybrid/Auth.php" );
  
 $hybridauth = new Hybrid_Auth( $config_file_path );
 
