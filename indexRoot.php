e955ee84bf2df7ca58f762bee8e679476e0c918e
pablitomillenios <65786238+pablitomillenios@users.noreply.github.com>
2020-12-27 13:12:04 +0100

    signin page

---
+++ indexRoot.php
@@ -0,0 +1,9 @@
+<body>
+Free-OKR 
+
+Objectives and Key Results
+
+<a href=/login_social_media/loginLinkedIn.php><img src="signing-button-linkedin.png"></a>
+<br><br>
+<a href=/login_social_media/loginGoogle.php><img src="signing-button-google.png" width="220px"></a>
+</body>
